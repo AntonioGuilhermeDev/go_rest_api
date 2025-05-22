4b72b0d0e9cc3d481beecbc84bb209c725ec76ff
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-05-22 07:55:32 -0300

    Criando token JWT ao fazer login

--- routes/users.go
+++ routes/users.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/AntonioGuilhermeDev/go-rest-api/models"
+	"github.com/AntonioGuilhermeDev/go-rest-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -44,5 +45,11 @@ func login(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
+	token, err := utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate user"})
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "Login successful!", "token": token})
 }
